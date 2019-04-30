14ec25a81db1b0038348281abe6e7575c3600deb
yamatsuki <yamatsuki78@gmail.com>
2019-04-30 16:53:11 +0200

    add some functionnalities

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 #######################################
 ###### Déclaration des imports ########
@@ -9,7 +9,6 @@ from time import sleep
 from donnees import *
 from random import choice
 import pickle
-
 
 #########################################
 # Déclaration des variables pour le jeu #
@@ -22,98 +21,121 @@ testLetter = ""
 
 secretWord = []
 word = []
-
 discovered_letter_in_word = []
+
+
 #########################################
 # Déclaration des fonctions pour le jeu #
 #########################################
 
 def narrationPause(timepause):
-    """ une fonction permettant de faire des pauses pendant la narration du jeu """
-    sleep(timepause)
+	""" une fonction permettant de faire des pauses pendant la narration du jeu """
+	sleep(timepause)
+
 
 def welcome():
-    """ une fonction permettant d'annoncer le jeu et souhaiter la bienvenue """
-    print("Bienvenue au jeu du Pendu")
-    narrationPause(1)
+	""" une fonction permettant d'annoncer le jeu et souhaiter la bienvenue """
+	print("Bienvenue au jeu du Pendu")
+	narrationPause(1)
+
 
 def sayHello(gamer):
-    """ une fonction permettant de dire bonjour au joueur """
-    print("Bonjour {}, nous allons jouer ensemble".format(gamer))
-    narrationPause(1)
+	""" une fonction permettant de dire bonjour au joueur """
+	print("Bonjour {}, nous allons jouer ensemble".format(gamer))
+	narrationPause(1)
+
 
 def playerName():
-    """ une fonction renvoyant  le nom du joueur """
-    global my_name
-    my_name = input("Quel est votre nom: ? ")
-    if my_name == "":
-        print("ah bon ?")
-        return playerName()
-    else:
-        try:
-            my_name = int(my_name)
-            print('Vous devez rentrer une chaine de caractère')
-            return playerName()
+	""" une fonction renvoyant  le nom du joueur """
+	global my_name
+	my_name = input("Quel est votre nom: ? ")
+	if my_name == "":
+		print("ah bon ?")
+		return playerName()
+	else:
+		try:
+			my_name = int(my_name)
+			print('Vous devez rentrer une chaine de caractère')
+			return playerName()
 
-        except:
-            return my_name
-
+		except:
+			return my_name
 
 
 def importData():
-    """ une fonction renvoyant toutes le données necessaire au jeu """
-    wordToFind  = choice(wordList)
-    hits        = avaible_hits
-    return wordToFind, hits
+	""" une fonction renvoyant toutes le données necessaire au jeu """
+	wordToFind = choice(wordList)
+	hits = avaible_hits
+	return wordToFind, hits
 
 
 def userLetter():
-    """ une fonction renvoyant la lettre essayée par le joueur """
-    my_letter = input("Veuillez saisir une lettre : ")
-    if my_letter == "":
-        print("Soyez serieux, vous voulez jouer ?")
-        return userLetter()
-    elif len(my_letter) >1:
-        print("Enfiiiin !! Une seule lettre à la fois, svp ?")
-        return userLetter()
-    try:
-        my_letter = int(my_letter)
-        print('Vous devez rentrer une chaine de caractère')
-        return userLetter()
-    except:
-        return my_letter
+	""" une fonction renvoyant la lettre essayée par le joueur """
+	my_letter = input("Veuillez saisir une lettre : ")
+	if my_letter == "":
+		print("Soyez serieux, vous voulez jouer ?")
+		return userLetter()
+	elif len(my_letter) > 1:
+		print("Enfiiiin !! Une seule lettre à la fois, svp ?")
+		return userLetter()
+	try:
+		my_letter = int(my_letter)
+		print('Vous devez rentrer une chaine de caractère')
+		return userLetter()
+	except:
+		return my_letter
 
 
-def isLetterAlreayUsed(letter_to_test):
-    """ une fonction stockant les lettres essayées par le joueur """
-    global letter_already_used
+def isLetterAlreayUsed(letter_to_test, hits, wdtotest):
+	""" une fonction stockant les lettres essayées par le joueur """
+	global letter_already_used
 
-    if len(letter_already_used) == 0:
-        letter_already_used.append(letter_to_test)
-        return True
-    else:
-        for letter in letter_already_used:
-            if letter == letter_to_test:
-                print("Lettre déja recherchée")
-                print("Voici votre liste de lettre : ", letter_already_used)
-                return False
-            else:
-                letter_already_used.append(letter_to_test)
-                print("lettre déjà enregistrées : ", letter_already_used)
-                return True
+	if len(letter_already_used) == 0:
+		for letter in wdtotest:
+			if letter != letter_to_test:
+				letter_already_used.append(letter_to_test)
+				print("Désolé, la lettre ne se trouve pas dans le mot.")
+				hits -= 1
+				return hits, True
+			else:
+				letter_already_used.append(letter_to_test)
+				print("Bravo. Une lettre de trouvée")
+				return hits, True
+	else:
+		for letter in letter_already_used:
+			if letter == letter_to_test:
+				print("Lettre déja recherchée")
+				print("Voici votre liste de lettre : ", letter_already_used)
+				hits -= 1
+				return hits, False
+			else:
+				for letter in wdtotest:
+					if letter != letter_to_test:
+						letter_already_used.append(letter_to_test)
+						hits -= 1
+						print("Désolé, la lettre ne se trouve toujours pas dans le mot.")
+						print("lettre déjà enregistrées : ", letter_already_used)
+						return hits, True
+					else:
+						letter_already_used.append(letter_to_test)
+						print("Bien. Une lettre de trouvée")
+						print("lettre déjà enregistrées : ", letter_already_used)
+						return hits, True
+
 
 def isGoodLetter(wtfd, user_letter):
-    global good_letter
-    for letter in wtfd:
-        if user_letter == letter:
-            good_letter += user_letter
-    return good_letter
+	global good_letter
+	for letter in wtfd:
+		if user_letter == letter:
+			good_letter += user_letter
+	return good_letter
+
 
 def word_to_display(finding_word, good_letter):
-    word_to_display = ""
-    for letter in finding_word:
-        if letter in good_letter:
-            word_to_display += letter
-        else:
-            word_to_display += "*"
-    return word_to_display
+	word_to_display = ""
+	for letter in finding_word:
+		if letter in good_letter:
+			word_to_display += letter
+		else:
+			word_to_display += "*"
+	return word_to_display
--- pendu.py
+++ pendu.py
@@ -21,20 +21,27 @@ word_to_find = ""
 ######### Déroulement du jeu ############
 #########################################
 
+welcome()
+player_name = playerName()
+sayHello(player_name)
+
 while game_on:
     secret_word, hits = importData()
     print("{},{}".format(secret_word, hits))
-    welcome()
-    player_name = playerName()
-    sayHello(player_name)
     print("Voici le mot a trouver : ", word_to_display(secret_word, ""))
+    print("Vous avez {} coups: ".format(hits))
     # word_to_find = secret_word
-    while not is_secret_word_discovered and word_to_find != secret_word:
-        # word_to_find = secret_word
-        my_letter = userLetter().lower()
-        my_good_letter = isGoodLetter(secret_word, my_letter)
-        letterOk = isLetterAlreayUsed(my_letter)
-        if letterOk:
-            word_to_find = word_to_display(secret_word, my_good_letter)
-            # word_to_find = displaySecretWord(secret_word)
-            print("pendu mot a trouver: ", word_to_find)
+    while not is_secret_word_discovered:
+        if word_to_find != secret_word and hits > 0:
+            # word_to_find = secret_word
+            my_letter = userLetter().lower()
+            my_good_letter = isGoodLetter(secret_word, my_letter)
+            hits, letterOk = isLetterAlreayUsed(my_letter, hits, secret_word)
+            if letterOk:
+                word_to_find = word_to_display(secret_word, my_good_letter)
+                # word_to_find = displaySecretWord(secret_word)
+                print("mot a trouver: ", word_to_find)
+                print("Il vous reste {} coups: ".format(hits))
+            else:
+                print("mot a trouver: ", word_to_find)
+                print("Il vous reste {} coups: ".format(hits))
